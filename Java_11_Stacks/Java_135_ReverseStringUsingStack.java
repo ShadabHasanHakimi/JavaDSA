a08beb283bf376515f2ceb8878509861f8eedc74
ShadabHasanHakimi <shadabhasan786123@gmail.com>
2023-10-21 07:50:02 +0530

    Commited

--- Java_11_Stacks/Java_135_ReverseStringUsingStack.java
+++ Java_11_Stacks/Java_135_ReverseStringUsingStack.java
@@ -6,12 +6,12 @@ public class Java_135_ReverseStringUsingStack {
     public static String reverse(String str){
         Stack<Character> s = new Stack<>();
         int idx = 0;
-        while(idx<str.length()){
+        while(idx<str.length()){    // Copying the string into stack
             s.push(str.charAt(idx));
             idx++;
         }
         StringBuilder res = new StringBuilder();
-        while(!s.isEmpty()){
+        while(!s.isEmpty()){    // Copying reverse of string using pop in sb
             res.append(s.pop());
         }
         return res.toString();
---
+++ Java_11_Stacks/Java_136_StockSpanProblem.java
@@ -0,0 +1,8 @@
+package JavaDSA.Java_11_Stacks;
+
+public class Java_136_StockSpanProblem {
+    public static void stockSpan(){
+
+    }
+
+}
