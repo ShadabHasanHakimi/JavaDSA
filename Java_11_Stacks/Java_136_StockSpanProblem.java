b488227b3b6943248f73f5eb7fb223f17c24485b
ShadabHasanHakimi <shadabhasan786123@gmail.com>
2023-10-24 08:27:58 +0530

    Commited

--- Java_11_Stacks/Java_136_StockSpanProblem.java
+++ Java_11_Stacks/Java_136_StockSpanProblem.java
@@ -1,8 +1,34 @@
 package JavaDSA.Java_11_Stacks;
 
-public class Java_136_StockSpanProblem {
-    public static void stockSpan(){
+import java.util.Stack;
 
+public class Java_136_StockSpanProblem {
+    public static void stockSpan(int[] stocks, int[] span){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for(int i=1; i<stocks.length; i++){
+            int currPrice = stocks[i];
+            while(!s.isEmpty() && currPrice>stocks[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){    // like last element in our example
+                span[i] = i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i-prevHigh;
+            }
+            s.push(i);
+        }
     }
 
+    public static void main(String[] args) {
+        int[] stocks = {100, 80, 60, 70, 60, 85, 100};
+        int[] span = new int[stocks.length];
+        stockSpan(stocks, span);
+        for(int i=0; i<span.length; i++){
+            System.out.println(span[i]+" ");
+        }
+    }
 }
