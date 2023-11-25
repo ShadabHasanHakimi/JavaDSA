357651d05b37b25130e2bf01c92600ed403d6f86
ShadabHasanHakimi <shadabhasan786123@gmail.com>
2023-11-25 08:39:34 +0530

    Commited

---
+++ Java_12_Queue/Java_147_QFirstNonRepeating.java
@@ -0,0 +1,31 @@
+package JavaDSA.Java_12_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Java_147_QFirstNonRepeating {
+    public static void nonRepeating(String s){
+        int[] freq = new int[26];
+        Queue<Character> q = new LinkedList<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch - 'a']++;
+            while(!q.isEmpty() && freq[q.peek() - 'a'] > 1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.println(-1 + " ");
+            }
+            else{
+                System.out.println(q.peek()+" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String s = "aabccxb";
+        nonRepeating(s);
+    }
+}
